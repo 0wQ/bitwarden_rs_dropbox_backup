ae56b2f2f2194a50398b928847dfe86325a75f10
Mizore <xvz@live.com>
2021-02-17 16:39:31 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,7 +7,12 @@ BACKUP_FILE="bitwardenrs_$(date "+%F-%H%M%S")"
 sqlite3 /data/db.sqlite3 ".backup '/tmp/db.sqlite3'"
 
 # tar up backup and encrypt with openssl and encryption key
-tar -czf - /tmp/db.sqlite3 /data/attachments | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE}.tar.gz
+if [ -z $BACKUP_ENCRYPTION_KEY ]
+then
+  tar -cz /tmp/db.sqlite3 /data/attachments -f /tmp/${BACKUP_FILE}.tar.gz
+else
+  tar -czf - /tmp/db.sqlite3 /data/attachments | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE}.tar.gz
+fi
 
 # upload encrypted tar to dropbox
 /dropbox_uploader.sh -f /config/.dropbox_uploader upload /tmp/${BACKUP_FILE}.tar.gz /${BACKUP_FILE}.tar.gz
